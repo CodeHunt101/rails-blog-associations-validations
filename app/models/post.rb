cc7d23bc3b0f2b528b53d8d5704e769eff11623e
Harold Torres Marino <haroldtm55@gmail.com>
2021-10-16 17:10:26 +1100

    Lab completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ActiveRecord::Base
-  
+  belongs_to :user
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+
+  validates :name, :content, presence: true
+
+  def tag_names
+    self.tags.map{|t| t.name}
+  end
 end
